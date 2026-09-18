3aca8764677d23793ff774d4cad164497220abce
namhyuk <lnh929@naver.com>
2026-09-19 01:10:38 +0900

    Initial Commit

---
+++ Sangil/SangilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SangilApp.swift
+//  Sangil
+//
+//  Created by namhyuk on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SangilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
